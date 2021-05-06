4ac67ce1cbb9c0393d21c14c7f4232aa75c2a3cc
Santi <zanti.f@gmail.com>
2021-05-06 11:21:31 -0300

    arrays exercises

---
+++ arrays/exercises/stringsAndArrays.js
@@ -0,0 +1,10 @@
+// 1. Take the following string and split it into an array.
+const tastyString = 'dragon - panther - whale - giant squid - take-out - cat dog';
+const tastyArray = tastyString.split(" - ");
+console.log (tastyArray);
+
+// 2. given array from part 1, join elements back together into a string.
+
+
+
+// 3. think of a way to, given a string, quickly reverse the string.
