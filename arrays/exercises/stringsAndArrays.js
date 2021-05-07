90b83010a54586f8da26c75e568920ff03c7a80c
Santi <zanti.f@gmail.com>
2021-05-06 21:18:16 -0300

    last exercices

--- arrays/exercises/stringsAndArrays.js
+++ arrays/exercises/stringsAndArrays.js
@@ -4,7 +4,14 @@ const tastyArray = tastyString.split(" - ");
 console.log (tastyArray);
 
 // 2. given array from part 1, join elements back together into a string.
-
+const stringAgain = tastyArray.join(", ");
+console.log (stringAgain);
 
 
 // 3. think of a way to, given a string, quickly reverse the string.
+const stringy = "Santiago Alejandro De La Vega";
+var reverseStringy = "";
+for (let i = stringy.length-1; i >= 0; i--){
+  reverseStringy += stringy[i];
+}
+console.log(reverseStringy);
