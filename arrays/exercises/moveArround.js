463a012d66e49acafb0fe33de7e520b60a056f3a
Santi <zanti.f@gmail.com>
2021-05-07 23:46:42 -0300

    arrays exercises

--- arrays/exercises/moveArround.js
+++ arrays/exercises/moveArround.js
@@ -1,7 +1,17 @@
+var readlineSync = require('readline-sync');
+
+// ask user for their player name
+var userName = readlineSync.question('Enter player name: ');
+
 // make world for user
-const worldWidth = 10
+const worldWidth = 3
 const world = ' '.repeat(worldWidth).split('');
+console.log('Empty world');
 console.log(world);
 
 // place user
 const position = Math.floor(worldWidth / 2);
+world[position] = 'P';
+console.log('Player joined');
+console.log(world);
+
