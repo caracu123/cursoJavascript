fcc3e61548101e23362046aee2ffc32796b26098
santi <zanti.f@gmail.com>
2021-06-05 00:18:06 -0300

    objects exercises

--- objets/exercises/turnBasedFighter.js
+++ objets/exercises/turnBasedFighter.js
@@ -12,64 +12,104 @@ var playerName = readlineSync.question('Choose your name: ');
 var playerHP = +readlineSync.question('Insert your Health Points : ');
 var enemyName = readlineSync.question("Choose your enemy's name: ");
 
+// create player object
 var player = {
   name: playerName,
   hp: playerHP,
-  damage: playerHP * 0.2,
+  damage: playerHP * 0.3,
 };
-
+//  create enemy object
 var enemy = {
   name: enemyName,
-  hp: playerHP * 1.5,
-  damage: player.damage * 1.5,  
+  hp: playerHP * 1.2,
+  damage: player.damage * 1.1,  
 };
 
 console.log();
+console.log('---------------------------------------------------------');
 console.log('This is your character: ', player);
 console.log();
+console.log('---------------------------------------------------------');
 console.log('You need to defeat: ', enemy);
 console.log();
 
-// attack function
-function attack (attacker, reciever) {
+// player attack function
+function attack () {
   let chance = Math.random();
-  if (chance > 0.5) {
-    reciever.hp -= attacker.damage;
-    console.log(`You hit ${enemy.name} for ${attacker.damage} damage.`);
+  if (chance > 0.3) {
+    enemy.hp -= player.damage;
+    console.log(`You hit ${enemy.name} for ${player.damage} damage.`);
   }
   else console.log(`Attack missed, ${enemy.name} is too fast!`);
 }
 
-// super attack function
-function superAttack (attacker, reciever) {
+// player super attack function
+function superAttack () {
   let chance = Math.random();
-  if (chance > 0.4) {
-    damage = attacker.damage*2;
-    reciever.hp -= damage;
-    console.log(`You hit ${enemy.name} for ${damage}, but you also lost ${attacker.damage * 0.5} Health.`);
+  if (chance > 0.2) {
+    damage = player.damage*2;
+    enemy.hp -= damage;
+    console.log(`You hit ${enemy.name} for ${damage}, but you also lost ${player.damage * 0.5} Health.`);
   }
-  else console.log(`Super attack missed, you hurt yourself. -${attacker.damage * 0.5} health.`);
-  attacker.hp -= attacker.damage *0.5;
+  else console.log(`Super attack missed, you hurt yourself. -${player.damage * 0.5} Health.`);
+  player.hp -= player.damage *0.5;
 }
 
 // heal function
-function heal (someone) {
-  let heal = Math.random() * someone.damage;
-  someone.hp += heal;
+function heal () {
+  let heal = Math.random() * player.damage;
+  player.hp += heal;
   console.log(`You healed yourself for ${heal}, refreshing.`);
 }
 
 // enemy attack function
-function enemyAttack (player) {
+function enemyAttack () {
   let chance = Math.random();
-  if (chance > 0.5) {
+  if (chance > 0.3) {
     player.hp -= enemy.damage;
-    console.log(`${enemy.name} hit you for ${enemy.damage}`)
+    console.log(`${enemy.name} attacked and hit you for ${enemy.damage}`)
   }
+  else console.log(`${enemy.name} attacked and missed!`)
 }
 
+// enemy super attack function
+function enemySuperAttack () {
+  let chance = Math.random();
+  if (chance > 0.2) {
+    damage = enemy.damage*1.5;
+    player.hp -= damage;
+    enemy.hp -= enemy.damage *0.5;
+    console.log(`${enemy.name} made a super attack and hit you for ${damage}, but also lost ${enemy.damage *0.5} Health.`);
+  }
+  else console.log(`${enemy.name} super attack missed, lucky you! ${enemy.name} lost ${enemy.damage*0.5} Health`)
+}
+// loop while both hp are > than 0
 while (player.hp > 0 && enemy.hp > 0) {
   var playerAction = readlineSync.question(
-    '"AT" to attack, \n"SA" for a Super Attack, \n"HE" to heal \nInsert Next Move: '
-    );
+    '"A" to attack, \n"S" for Super Attack, \n"H" to heal \n"FF" to give up, \nInsert Next Move: '
+    ).toUpperCase();
+  console.log();
+  // player action
+  if (playerAction === 'A') attack();
+  else if (playerAction === 'S') superAttack();
+  else if (playerAction === 'H') heal();
+  else if (playerAction === 'FF') break;
+  else console.log('Invalid input, try again.');
+  console.log();
+  
+  // decide enemy movement
+  let enemyMove = Math.random();
+  if (enemyMove < 0.5) enemyAttack();
+  else enemySuperAttack();
+  console.log();
+    console.log(player.name, player.hp);
+  console.log();
+  console.log(enemy.name, enemy.hp);
+  console.log('---------------------------------------------------------');
+  console.log();
+  console.log();
+  console.log();
+  if (player.hp > 0 && enemy.hp < 0) console.log(`YOU WON, CONGRATS!!`);
+  else if (player.hp < 0 && enemy.hp < 0) console.log(`It's a DRAW, well played.`)
+  else console.log(`You lost HAHAHA!`);
 }
