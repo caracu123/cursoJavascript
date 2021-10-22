54e74b935a1113e393f829bd54bc6257b6642be8
santi <zanti.f@gmail.com>
2021-10-21 23:24:49 -0300

    last push

---
+++ arrayMethods/forEachRevew.js
@@ -0,0 +1,17 @@
+/*
+EXERCISE 1
+write a forEach taht, given an array of integers, prints out the cube of each element.
+[3, 6, 8].forEach(?)
+--------------------
+27
+216
+512
+
+EXERCISE 2
+write a function that given an array of numbers, sums up each number in the array.
+[3, 6, -18, 24.3].forEach(?)
+--------------------
+3 + 6 + -18 + 24.3
+15.3
+*/
+
